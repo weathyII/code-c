e9b10b713a821f210e35aec9b9fc7221ceb83891
方思 <3081195079@qq.com>
2023-10-22 01:14:09 +0800

    git add . git commit -m "first commit" git push origin master

---
+++ c++/practice.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class person
+{
+    bool operator==(const person &p)
+    {
+        
+    }
+}
+void test01()
+{
+    person s1;
+
+
+}
+int main()
+{
+    pero
+    return 0;
+}
